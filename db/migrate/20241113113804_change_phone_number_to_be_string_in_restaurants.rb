6b5b6637c64afc41f264e170b8f2aa27e1fabbae
JeanRemi Charrier <jeanremi.char@gmail.com>
2024-11-13 14:44:28 +0100

    yelp mvp done

---
+++ db/migrate/20241113113804_change_phone_number_to_be_string_in_restaurants.rb
@@ -0,0 +1,4 @@
+class ChangePhoneNumberToBeStringInRestaurants < ActiveRecord::Migration[7.1]
+  def change
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,22 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+puts "Destroy all..."
+Restaurant.destroy_all
+puts "Creating 5 restaurants ..."
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  restaurant.save!
+  5.times do
+    Review.create(
+      content: Faker::Restaurant.review,
+      rating: rand(0..5),
+      restaurant: restaurant
+    )
+  end
+end
+puts "Restaurants created !!!"
